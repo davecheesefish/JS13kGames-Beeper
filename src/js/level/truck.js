cc8bb30410a583072cdc2e1e1a4ce7dddb19b96c
David Prior <david.prior@davecheesefish.com>
2015-09-12 16:11:28 +0100

    Implemented full truck movement.

--- src/js/level/truck.js
+++ src/js/level/truck.js
@@ -1,11 +1,13 @@
 define(['utils/vector2', 'utils/rectangle', 'utils/inputhelper'], function(Vector2, Rectangle, Input){
 	
 	var Truck = function(position, rotation){
-		var maxSpeed = 20,
-			maxAcceleration = 20;
+		var maxSpeed = 60,
+			maxAcceleration = 20,
+			maxDeceleration = 30,
+			turnSpeed = 0.005 * Math.PI;
 		
 		var position = position || new Vector2(),
-			velocity = new Vector2(0, 0),
+			speed = 0,
 			rotation = rotation || 0,
 			boundingBox = new Rectangle(position.x, position.y, 26, 16, 0, 8, 8);
 		
@@ -17,10 +19,35 @@ define(['utils/vector2', 'utils/rectangle', 'utils/inputhelper'], function(Vecto
 		};
 		
 		this.update = function(deltaTime){
-			if (Input.keyIsPressed(38) && velocity.length() < maxSpeed){
-				velocity.add(Vector2.fromComponents(maxAcceleration * deltaTime, rotation));
+			if (Input.keyIsPressed(38) && speed < maxSpeed){
+				if (speed > 0) {
+					speed += maxAcceleration * deltaTime;
+				} else {
+					speed += maxDeceleration * deltaTime;
+				}
 			};
 			
+			if (Input.keyIsPressed(40) && speed > -maxSpeed){
+				if (speed > 0) {
+					speed -= maxDeceleration * deltaTime;
+				} else {
+					speed -= maxAcceleration * deltaTime;
+				}
+			};
+			
+			// Work out direction to turn
+			var turnFactor = 0;
+			if (Input.keyIsPressed(37)){
+				turnFactor--;
+			}
+			if (Input.keyIsPressed(39)){
+				turnFactor++;
+			}
+			
+			// Do the turning
+			rotation += turnFactor * turnSpeed * deltaTime * speed;
+			
+			var velocity = Vector2.fromComponents(speed, rotation);
 			position.add(Vector2.multiply(velocity, deltaTime));
 			this.updateBoundingBox();
 		};
